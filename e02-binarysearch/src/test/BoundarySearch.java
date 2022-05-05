5a54a2a2add6127e63f74363df5dc91b90caf355
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-05-05 23:15:48 +0200

    add basic tests

---
+++ e02-binarysearch/src/test/BoundarySearch.java
@@ -0,0 +1,64 @@
+package gad.binarysearch.tests.binarysearch;
+
+import gad.binarysearch.BinSea;
+import gad.binarysearch.StudentResult;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class BoundarySearch {
+	static Stream<Arguments> binarySearchBounds_Simple() {
+		return Stream.of(
+				Arguments.of(new int[]{1, 2, 3, 4}, 1, true, 0),
+				Arguments.of(new int[]{1, 2, 3, 4}, 2, true, 1),
+				Arguments.of(new int[]{1, 2, 3, 4}, 3, true, 2),
+				Arguments.of(new int[]{1, 2, 3, 4}, 3, false, 2)
+		);
+	}
+
+	static Stream<Arguments> binarySearchBounds_DuplicatesInArray() {
+		return Stream.of(
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, 2, true, 1),
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, 2, false, 3),
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, 6, true, 4),
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, 6, false, 4)
+		);
+	}
+
+	static Stream<Arguments> binarySearchBounds_ValueNotInArray() {
+		return Stream.of(
+				Arguments.of(new int[]{1, 2, 3, 4}, 5, true, -1),
+				Arguments.of(new int[]{1, 2, 3, 4}, 5, false, 3),
+				Arguments.of(new int[]{1, 2, 3, 4}, 0, true, 0),
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, 0, false, -1),
+				Arguments.of(new int[]{1, 2, 2, 2, 6, 7, 8, 9, 10}, -1, true, 0)
+		);
+	}
+
+	static Stream<Arguments> binarySearchBounds_RandomValues() {
+		return Stream.of(
+				Arguments.of(new int[]{0, 1, 1, 1, 1, 1, 1, 1}, 1, true, 1),
+				Arguments.of(new int[]{1, 1, 1, 1, 1, 1, 1}, 2, true, -1),
+				Arguments.of(new int[]{1, 1, 1, 1, 1, 1, 1, 4}, 1, false, 6),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8}, 4, true, 1),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8}, 5, true, 4),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8}, 5, false, 7),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8}, 9, false, 8),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8, 10, 23}, 6, true, 8),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8, 10, 23}, 6, false, 7),
+				Arguments.of(new int[]{1, 4, 4, 4, 5, 5, 5, 5, 8}, 9, true, -1)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource({"binarySearchBounds_Simple", "binarySearchBounds_RandomValues",
+			"binarySearchBounds_ValueNotInArray", "binarySearchBounds_DuplicatesInArray"})
+	void testSearch(int[] arr, int value, boolean lowerBound, int expectedIdx) {
+		assertEquals(expectedIdx, BinSea.search(arr, value, lowerBound, new StudentResult()));
+	}
+}
