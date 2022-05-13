f11512b03090095b13cba6dce9ec18f69007b0ef
Tobias <tobi.eppacher@gmail.com>
2022-05-13 20:01:33 +0200

    Added noot tests

--- e03-dynamicarray/src/test/NootTest.java
+++ e03-dynamicarray/src/test/NootTest.java
@@ -1,13 +1,14 @@
 package test;
 
 import gad.dynamicarray.Noot;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NootTest {
 
@@ -49,7 +50,7 @@ class NootTest {
 
     @ParameterizedTest
     @MethodSource({"popThenPush", "simpleExample"})
-    void artemisTest(Noot noot, NootOperation op, int value, int expectedSizeAfter) {
+    void basicTest(Noot noot, NootOperation op, int value, int expectedSizeAfter) {
         switch (op) {
             case PUSH -> noot.pushBack(value);
             case POP_BACK -> assertEquals(value, noot.popBack());
@@ -57,4 +58,152 @@ class NootTest {
         }
         assertEquals(expectedSizeAfter, noot.size());
     }
+
+
+
+
+    ///////////////////////
+
+    @Test
+        public void popFrontToEmpty() {
+            Noot noot = new Noot(3, 4);
+
+            assertTrue(noot.isEmpty());
+            noot.pushBack(4);
+            noot.pushBack(3);
+            noot.pushBack(2);
+            noot.pushBack(1);
+
+            assertEquals(4, noot.popFront());
+            assertEquals(3, noot.popFront());
+            assertEquals(2, noot.popFront());
+            assertEquals(1, noot.popFront());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popFront());
+            assertTrue(noot.isEmpty());
+        }
+
+        @Test
+        public void popBackToEmpty() {
+            Noot noot = new Noot(3, 4);
+
+            assertTrue(noot.isEmpty());
+            noot.pushBack(4);
+            noot.pushBack(3);
+            noot.pushBack(2);
+            noot.pushBack(1);
+
+            assertEquals(1, noot.popBack());
+            assertEquals(2, noot.popBack());
+            assertEquals(3, noot.popBack());
+            assertEquals(4, noot.popBack());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popBack());
+            assertTrue(noot.isEmpty());
+        }
+
+        @Test
+        public void popMixed() {
+            Noot noot = new Noot(3, 4);
+
+            noot.pushBack(1);
+            noot.pushBack(2);
+            noot.pushBack(3);
+            noot.pushBack(4);
+
+            assertEquals(1, noot.popFront());
+            assertEquals(4, noot.popBack());
+            assertEquals(3, noot.popBack());
+            assertEquals(2, noot.popBack());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popFront());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(101);
+            assertEquals(101, noot.popFront());
+            assertTrue(noot.isEmpty());
+        }
+
+        @Test
+        public void popMixed2() {
+            Noot noot = new Noot(2, 5);
+
+            noot.pushBack(1);
+            noot.pushBack(2);
+            noot.pushBack(3);
+            noot.pushBack(4);
+
+            assertEquals(1, noot.popFront());
+            assertEquals(2, noot.popFront());
+            assertEquals(3, noot.popFront());
+            assertEquals(4, noot.popBack());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popFront());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(101);
+            assertEquals(101, noot.popBack());
+            assertTrue(noot.isEmpty());
+        }
+
+        @Test
+        public void popMixed3() {
+            Noot noot = new Noot(2, 5);
+
+            noot.pushBack(1);
+            assertEquals(1, noot.popFront());
+
+            noot.pushBack(2);
+            noot.pushBack(3);
+            assertEquals(3, noot.popBack());
+            noot.pushBack(4);
+
+            assertEquals(2, noot.popFront());
+            noot.pushBack(69);
+            assertEquals(4, noot.popFront());
+            assertEquals(69, noot.popBack());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popFront());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(101);
+            assertEquals(101, noot.popBack());
+            assertTrue(noot.isEmpty());
+        }
+
+        @Test
+        public void popMixed4() {
+            Noot noot = new Noot(2, 5);
+
+            noot.pushBack(1);
+            noot.pushBack(2);
+            assertEquals(1, noot.popFront());
+            noot.pushBack(3);
+            assertEquals(3, noot.popBack());
+            noot.pushBack(4);
+            noot.pushBack(69);
+            assertEquals(2, noot.popFront());
+            assertEquals(69, noot.popBack());
+            assertEquals(4, noot.popFront());
+
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(42);
+            assertEquals(42, noot.popFront());
+            assertTrue(noot.isEmpty());
+
+            noot.pushBack(101);
+            assertEquals(101, noot.popBack());
+            assertTrue(noot.isEmpty());
+        }
 }
